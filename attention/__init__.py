becb979975b3c62c82f6a096d6eea8d300375b7a
Naura Kasihna Risat <naurarisat11@gmail.com>
2026-09-15 22:35:10 +0700

    Delete attention directory

--- attention/__init__.py
+++
@@ -1,5 +0,0 @@
-from .multi_head import MultiHeadedAttention
-from .single import Attention
-from .transformer import TransformerBlock
-
-__all__ = ['MultiHeadedAttention', 'Attention', 'TransformerBlock']
--- attention/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .sublayer import SublayerConnection
-from .feed_forward import PositionwiseFeedForward
-from .gelu import GELU
-from .layer_norm import LayerNorm
-
-__all__ = ['SublayerConnection', 'PositionwiseFeedForward', 'GELU', 'LayerNorm']
